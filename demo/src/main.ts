4fe29c48cf8d9b8f486d0e27b6c0052c7985b985
spencerchubb <spencerchubb@gmail.com>
2023-02-16 01:06:26 -0500

    different way of importing wasm

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,11 +1,4 @@
-// import { init } from "@spencerchubb/solver";
-
-// init("./pkg/solver_wasm.js").then(wasmObj => {
-//     const scramble = wasmObj.scramble("R U R' U' R' F R2 U' R' U' R U R' F'");
-//     console.log({ scramble });
-// });
-
-import { scramble as wasmScramble } from "solver-wasm";
+import { scramble as wasmScramble } from "./pkg/solver_wasm";
 
 const scramble = wasmScramble("R U R' F' R U R' U' R' F R2 U' R' U'");
 console.log({ scramble });
