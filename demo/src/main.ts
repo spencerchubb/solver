c3bc2c8080346b715ed31c026dbb24ddf89f6092
spencerchubb <spencerchubb@gmail.com>
2023-02-20 19:59:25 -0500

    update demo

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,4 +1,8 @@
-import { scramble as wasmScramble } from "@spencerchubb/solver";
+import { scramble } from "@spencerchubb/solver";
 
-const scramble = wasmScramble("R U R' F' R U R' U' R' F R2 U' R' U'");
-console.log({ scramble });
+const alg = "R U R' F' R U R' U' R' F R2 U' R' U'";
+const moves = "U,U',F,F',R,R'";
+const onlyOrientation: number[] = [];
+const disregard: number[] = [];
+const scrambleFound = scramble(alg, moves, onlyOrientation, disregard);
+console.log({ scrambleFound });
