1508295c6280e7a505a13e75cd9800209e12f2d3
spencerchubb <spencerchubb@gmail.com>
2023-02-25 21:48:53 -0500

    config for dev and prod

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -2,17 +2,20 @@ import { defineConfig } from 'vite'
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
-export default defineConfig({
-    minify: false,
-    plugins: [
-        wasm(),
-        topLevelAwait(),
-    ],
-    worker: {
-        format: "es",
+export default defineConfig(({ command, mode, ssrBuild }) => {
+    return {
+        base: mode === 'development' ? '/' : '/solver/demo/',
+        minify: false,
         plugins: [
             wasm(),
             topLevelAwait(),
         ],
-    },
+        worker: {
+            format: "es",
+            plugins: [
+                wasm(),
+                topLevelAwait(),
+            ],
+        },
+    };
 });
