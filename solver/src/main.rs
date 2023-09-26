f8524f769266690ab8f42f0264449a7bf1f30517
spencerchubb <spencerchubb@gmail.com>
2023-09-26 09:12:28 -0400

    0.1.9

--- solver/src/main.rs
+++ solver/src/main.rs
@@ -16,10 +16,16 @@ pub fn main() {
     // let disregard: &[usize] = &[];
 
     // Find cross solution
-    let alg = "L2 D2 F2 L' F2 R' F2 L2 B2 R B2 R2 B D F2 R' F' U B R2 B'";
+    // let alg = "L2 D2 F2 L' F2 R' F2 L2 B2 R B2 R2 B D F2 R' F' U B R2 B'";
+    // let moves = "U U' D D' R R' L L' F F' B B'";
+    // let only_orientation: &[usize] = &[];
+    // let disregard: &[usize] = &[0,1,2,3,4,5,6,7,12,13,14,15,16,17,18,19];
+
+    // EOLine
+    let alg = "L2 F2 L2 B R2 F' R2 U2 F' R2 U2 L2 R' U L2 F2 R' D' B2 R' U";
     let moves = "U U' D D' R R' L L' F F' B B'";
-    let only_orientation: &[usize] = &[];
-    let disregard: &[usize] = &[0,1,2,3,4,5,6,7,12,13,14,15,16,17,18,19];
+    let only_orientation: &[usize] = &[8, 9, 10, 11, 12, 13, 15, 16, 18, 19];
+    let disregard: &[usize] = &[0, 1, 2, 3, 4, 5, 6, 7];
 
     let mut start = crate::cube::Cube::new();
     let mut end = crate::cube::Cube::new();
